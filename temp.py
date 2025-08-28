dc627d655080e9c381de39476c1b5aa733f83248
congrats-hg <hagyeong929@gmail.com>
2025-08-28 23:56:33 +0900

    11

--- temp.py
+++ temp.py
@@ -1,10 +1,3 @@
-class Solution:
-    def checkIfPangram(self, sentence: str) -> bool:
-        sentence_to_set = set(list(sentence))
-        if len(sentence_to_set) == 26:
-            return True
-        else:
-            return False
 
 instance = Solution()
 print(instance.checkIfPangram("leetcode"))
